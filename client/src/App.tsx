a19310bd411037121ee636db6e661b39b20585af
Michael Van Hulle <Mike@TheComputer.com>
2025-08-06 12:37:12 -0700

    #20
    Added CORS.  Need to fix Typescript errors.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,8 @@ function App() {
     fetch('https://localhost:5001/api/activities')
       .then(response => response.json())
       .then(data => setActivities(data))
+
+      return () => {}
   }, [])
 
   return (
