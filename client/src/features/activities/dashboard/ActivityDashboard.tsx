96b29f207079f6ae7025678a015ecb40479e4527
Michael Van Hulle <Mike@TheComputer.com>
2025-08-07 21:29:18 -0700

    #46
    Added ActivityDetail.tsx and images.

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,20 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetail";
+import { act } from "react";
 
 type Props = {
     activities: Activity[]
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({ activities }: Props) {
     return (
-        <Grid container>
-            <Grid size={9}>
+        <Grid container spacing={3}>
+            <Grid size={7}>
                 <ActivityList activities={activities} />
+            </Grid>
+            <Grid size={5}>
+                {activities[0] && <ActivityDetail activity={activities[0]}/>}
             </Grid>
         </Grid>
     )
---
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -0,0 +1,27 @@
+import { Button, CardActions, CardContent, Typography } from "@mui/material"
+import Card from "@mui/material/Card"
+import CardMedia from "@mui/material/CardMedia"
+
+type Props = {
+    activity: Activity
+}
+
+export default function ActivityDetail({activity}: Props) {
+  return (
+    <Card sx={{borderRadius: 3}}>
+        <CardMedia 
+            component='img'
+            src={`/images/categoryImages/${activity.category}.jpg`}
+        />
+        <CardContent>
+            <Typography variant="h5">{activity.title}</Typography>
+            <Typography variant="subtitle1" fontWeight='light'>{activity.date}</Typography>
+            <Typography variant="body1" >{activity.description}</Typography>
+        </CardContent>
+        <CardActions>
+            <Button color="primary">Edit</Button>
+            <Button color="inherit">Cancel</Button>
+        </CardActions>
+    </Card>
+  )
+}
