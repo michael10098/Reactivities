22cc895cd8abc5dcff164a48d99db401d607a5d6
Michael Van Hulle <Mike@TheComputer.com>
2025-08-08 15:17:32 -0700

    #51
    Saving an activity.

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,6 +11,7 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean
+    submitForm: (acitivity: Activity) => void
 }
 
 export default function ActivityDashboard({ 
@@ -20,7 +21,8 @@ export default function ActivityDashboard({
         selectedActivity,
         openForm,
         closeForm,
-        editMode
+        editMode,
+        submitForm
      }: Props) {
     return (
         <Grid container spacing={3}>
@@ -39,7 +41,10 @@ export default function ActivityDashboard({
                     />
                 }
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+                <ActivityForm 
+                    closeForm={closeForm} activity={selectedActivity} 
+                    submitForm={submitForm}
+                />}
             </Grid>
         </Grid>
     )
