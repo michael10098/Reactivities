229e7268f4da486038e4ca7573f807831d6177bf
Michael Van Hulle <Mike@TheComputer.com>
2025-08-08 20:47:27 -0700

    #56
    Added React development tools.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -52,7 +52,7 @@ function App() {
   }
 
   return (
-    <Box sx={{ bgcolor: '#eeeeee' }}>
+    <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
       <CssBaseline />
       <NavBar openForm={handleOpenFrom} />
       <Container maxWidth='xl' sx={{ mt: 3 }}>
