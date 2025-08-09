6042b999da416e7e5a918e3c394f5fda65c436cc
Michael Van Hulle <Mike@TheComputer.com>
2025-08-09 14:19:28 -0700

    #61
    Added the ability to create and activity.

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,30 +1,42 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 export const useActivities = () => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const { data: activities, isPending } = useQuery({
-    queryKey: ['activities'],
-    queryFn: async () => {
-      const response = await agent.get<Activity[]>('/activities');
-      return response.data
+    const { data: activities, isPending } = useQuery({
+        queryKey: ['activities'],
+        queryFn: async () => {
+            const response = await agent.get<Activity[]>('/activities');
+            return response.data
+        }
+    })
+
+    const updateActivity = useMutation({
+        mutationFn: async (activity: Activity) => {
+            await agent.put('/activities', activity)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    const createActivity = useMutation({
+        mutationFn: async (activity: Activity) => {
+            await agent.post('/activities', activity)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    return {
+        activities,
+        isPending,
+        updateActivity,
+        createActivity
     }
-  })
-
-  const updateActivity = useMutation({
-    mutationFn: async (activity: Activity) => {
-        await agent.put('/activities', activity)
-    },
-    onSuccess: async () => {
-        await queryClient.invalidateQueries({
-            queryKey: ['activities']
-        })
-    }
-  })
-
-  return {
-    activities,
-    isPending,
-    updateActivity
-  }
 }
