68378223b3e381e568a6c3e5e6750e7ef0bee557
Michael Van Hulle <Mike@TheComputer.com>
2025-08-12 18:04:05 -0700

    #84
    using MobX.

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -4,6 +4,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import HomePage from "../../features/home/homePage";
 import ActivityDetailPage from "../../features/activities/details/ActivityDetailPage";
+import Counter from "../../features/counter/Counter";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +15,8 @@ export const router = createBrowserRouter([
             {path: 'activities', element: <ActivityDashboard />},
             {path: 'activities/:id', element: <ActivityDetailPage />},
             {path: 'createActivity', element: <ActivityForm key='create' />},
-            {path: 'manage/:id', element: <ActivityForm />}
+            {path: 'manage/:id', element: <ActivityForm />},
+            {path: 'counter', element: <Counter />}
         ]
     }
 ])
