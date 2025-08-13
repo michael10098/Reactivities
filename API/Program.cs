947be3ae58de865b592b476432462259cd6d010c
Michael Van Hulle <Mike@TheComputer.com>
2025-08-13 14:01:24 -0700

    #94
    Adding Validation middleware.

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,10 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddMediatR(x =>
-    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+{
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+    x.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 
