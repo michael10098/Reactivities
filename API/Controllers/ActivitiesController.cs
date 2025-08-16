76a67a9cc0d54a1d7b6829d2a2ff58b8b260470e
Michael Van Hulle <Mike@TheComputer.com>
2025-08-15 19:37:27 -0700

    #97
    Handling API error responses part 2.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -22,7 +22,7 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        return HandleResult(await Mediator.Send(new GetActivityDetails.Query { Id = id }));
     }
 
     [HttpPost]
