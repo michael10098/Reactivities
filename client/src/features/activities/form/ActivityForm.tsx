cc2d2eb62151c26c44aeaf4d31c44e1f8fed87c9
Michael Van Hulle <Mike@TheComputer.com>
2025-08-19 16:01:33 -0700

    #113
    Creating a reusable Select Input.

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
 import { zodResolver } from '@hookform/resolvers/zod'
 import TextInput from "../../../app/shared/components/TextInput";
+import SelectInput from "../../../app/shared/components/SelectInput";
+import { categoryOptions } from "./categoryOptions";
 
 export default function ActivityForm() {
   const { control, reset, handleSubmit } = useForm<ActivitySchema>({
@@ -33,8 +35,12 @@ export default function ActivityForm() {
       <Box component='form' onSubmit={handleSubmit(onSubmit)} display='flex' flexDirection='column' gap={3}>
         <TextInput label='Title' control={control} name='title' />
         <TextInput label='Description' control={control} name='description'
-            multiline rows={3} />
-        <TextInput label='Category' control={control} name='category' />
+          multiline rows={3} />
+        <SelectInput items={categoryOptions}
+          label='Category' 
+          control={control} 
+          name='category' 
+        />
         <TextInput label='Date' control={control} name='date' />
         <TextInput label='City' control={control} name='city' />
         <TextInput label='Venue' control={control} name='venue' />
