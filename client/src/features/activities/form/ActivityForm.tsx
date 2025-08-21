04165efb3c3a36b3baba4e404fbf816018c16abd
Michael Van Hulle <Mike@TheComputer.com>
2025-08-20 17:47:23 -0700

    #118
    Form submission
    
    To resolve an error, I had to change useActivities.ts so it uses type FieldValues.

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useForm } from 'react-hook-form';
 import { useEffect } from "react";
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
@@ -16,15 +16,38 @@ export default function ActivityForm() {
     mode: 'onTouched',
     resolver: zodResolver(activitySchema)
   });
+  const navigate = useNavigate();
   const { id } = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
 
   useEffect(() => {
-    if (activity) reset(activity)
+    if (activity) reset({
+      ...activity,
+      location: {
+        city: activity.city,
+        venue: activity.venue,
+        latitude: activity.latitude,
+        longitude: activity.longitude
+      }
+    })
   }, [activity, reset])
 
   const onSubmit = async (data: ActivitySchema) => {
-    console.log(data)
+    const {location, ...rest} = data;
+    const flattenedData = {...rest, ...location};
+    try {
+      if (activity) {
+        updateActivity.mutate({...activity, ...flattenedData}, {
+          onSuccess: () => navigate(`/activities/${activity.id}`)
+        })
+      } else {
+        createActivity.mutate(flattenedData, {
+          onSuccess: (id) => navigate(`/activities/${id}`)
+        })
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   if (isLoadingActivity) return <Typography>Loading activity...</Typography>
@@ -38,12 +61,14 @@ export default function ActivityForm() {
         <TextInput label='Title' control={control} name='title' />
         <TextInput label='Description' control={control} name='description'
           multiline rows={3} />
-        <SelectInput items={categoryOptions}
-          label='Category' 
-          control={control} 
-          name='category' 
-        />
-        <DateTimeInput label='Date' control={control} name='date' />
+        <Box display='flex' gap={3}>
+          <SelectInput items={categoryOptions}
+            label='Category'
+            control={control}
+            name='category'
+          />
+          <DateTimeInput label='Date' control={control} name='date' />
+        </Box>
 
         <LocationInput control={control} label='Enter the location' name="location" />
 
--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,6 +1,9 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
+import { type FieldValues } from "react-hook-form";
+
+
 export const useActivities = (id?: string) => {
     const queryClient = useQueryClient();
     const location = useLocation();
@@ -38,7 +41,7 @@ export const useActivities = (id?: string) => {
     })
 
     const createActivity = useMutation({
-        mutationFn: async (activity: Activity) => {
+        mutationFn: async (activity: FieldValues) => {
             const response = await agent.post('/activities', activity);
             return response.data;
         },
