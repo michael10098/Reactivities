810b3ad47bd8731dea57e281ae83330f9f16c3cb
Michael Van Hulle <Mike@TheComputer.com>
2025-08-21 13:44:11 -0700

    #133
    Creating a user menu

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,9 +1,11 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { LoginSchema } from "../schemas/loginSchema"
 import agent from "../api/agent";
+import { useNavigate } from "react-router";
 
 export const useAccount = () => {
     const queryClient = useQueryClient();
+    const navigate = useNavigate();
 
     const loginUser = useMutation({
         mutationFn: async (creds: LoginSchema) => {
@@ -13,6 +15,18 @@ export const useAccount = () => {
             await queryClient.invalidateQueries({
                 queryKey: ['user']
             })
+            await navigate('/activities')
+        }
+    })
+
+    const logoutUser = useMutation({
+        mutationFn: async () => {
+            await agent.post('/account/logout');
+        },
+        onSuccess: () => {
+            queryClient.removeQueries({queryKey: ['user']});
+            queryClient.removeQueries({queryKey: ['activities']});
+            navigate('/')
         }
     })
 
@@ -21,11 +35,13 @@ export const useAccount = () => {
         queryFn: async () => {
             const response = await agent.get<User>('/account/user-info');
             return response.data;
-        }
+        },
+        enabled: !queryClient.getQueryData(['user'])
     })
 
     return {
         loginUser,
-        currentUser
+        currentUser,
+        logoutUser
     }
 }
