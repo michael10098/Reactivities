203183689b13f5f4df6c789a5114afaf9a7b688a
Michael Van Hulle <Mike@TheComputer.com>
2025-09-25 11:49:26 -0700

    #181
    Adding React Dropzone.

--- client/src/app/shared/components/PhotoUploadWidget.tsx
+++ client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,17 +1,40 @@
-import { Grid, Typography } from "@mui/material";
+import { CloudUpload } from "@mui/icons-material";
+import { Box, Grid, Typography } from "@mui/material";
+import { useCallback } from "react";
+import { useDropzone } from 'react-dropzone';
 
 export default function PhotoUploadWidget() {
-  return (
-    <Grid container spacing={3}>
-        <Grid size={4}>
-            <Typography variant="overline" color="secondary">Step 1 - Add Photo</Typography>
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        console.log(acceptedFiles);
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+    return (
+        <Grid container spacing={3}>
+            <Grid size={4}>
+                <Typography variant="overline" color="secondary">Step 1 - Add Photo</Typography>
+                <Box {...getRootProps()}
+                    sx={{
+                        border: 'dashed 3px #eee',
+                        borderColor: isDragActive ? 'green' : '#eee',
+                        borderRadius: '5px',
+                        paddingTop: '30px',
+                        textAlign: 'center',
+                        height: '280px'
+                    }}
+                >
+                    <input {...getInputProps()} />
+                    <CloudUpload sx={{fontSize: 80}} />
+                    <Typography variant="h5">Drop image here</Typography>
+                </Box>
+            </Grid>
+            <Grid size={4}>
+                <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
+            </Grid>
+            <Grid size={4}>
+                <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
+            </Grid>
         </Grid>
-        <Grid size={4}>
-            <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
-        </Grid>
-        <Grid size={4}>
-            <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
-        </Grid>
-    </Grid>
-  )
+    )
 }
