9cad63a56b89d19235953db81c9d6e13395e3867
Michael Van Hulle <Mike@TheComputer.com>
2025-10-01 17:06:12 -0700

    #220 and #221
    Challenge solution
    
    End of section 21.

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -63,4 +63,11 @@ public class ProfilesController : BaseApiController
             Predicate = predicate
         }));
     }
+
+    [HttpGet("{userId}/activities")]
+    public async Task<ActionResult<List<UserActivityDto>>> GetUserActivities(string userId, string filter)
+    {
+        return HandleResult(await Mediator.Send(new GetUserActivities.Query
+            { UserId = userId, Filter = filter }));
+    }
 }
